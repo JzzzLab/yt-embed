5568b648e02fdb47be1aca2723cf0f95ee976148
Jzzz <jzhung1013+jzzzlab@gmail.com>
2020-08-09 18:22:26 +0800

    new list or loop mode

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ function keyUpInput() {
 
 function getVideo() {
     var yt = "https://www.youtube.com/embed/";
-    var rel = "?rel=0"
+    var relList = "?rel=0&autoplay=1&list=";
+    var relLoop = "?rel=0&autoplay=1&loop=1&playlist=";
+    
     const inputValue = document.getElementById("myInput").value;
     var video = document.getElementById("video");
-
+    var isLoop = document.getElementById("myCheck").checked;
+    
     if(inputValue.length < 15) {
-        video.setAttribute("src", (yt + inputValue + rel));
+        video.setAttribute("src", (yt + inputValue + relLoop + inputValue));
     }else {
         var inputURL = new URL(inputValue);
         var vid = inputURL.searchParams.get("v");
-        video.setAttribute("src", (yt + vid + rel));
+        var lid = inputURL.searchParams.get("list");
+        if(isLoop) {
+            video.setAttribute("src", (yt + vid + relLoop + vid));
+        }else {
+            video.setAttribute("src", (yt + vid + relList + lid));
+        }
     }
 }
